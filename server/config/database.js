9bb8d914c4fee2ac404572ab02ce9f98556d786c
KhushiGulati16 <kgulati849@gmail.com>
2026-04-15 15:04:52 +0530

    Database file edit

--- server/config/database.js
+++ server/config/database.js
@@ -1,24 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: false, // Set to console.log to see SQL queries
-        define: {
-            timestamps: true
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'mysql',
+    logging: false,
+
+    // 👇 ADD IT HERE
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
     }
-);
+});
 
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
-        console.log('MySQL connected via Sequelize...');
+        console.log('MySQL connected via Sequelize ✅');
     } catch (err) {
         console.error('Unable to connect to the database:', err.message);
         process.exit(1);
